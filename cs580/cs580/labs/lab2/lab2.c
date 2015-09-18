7c4a42e0447e2e3759ca792c0f49a2b8a1d51133
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-09-17 21:16:51 -0400

    PST Ass-2 Submitted

--- cs580/cs580/labs/lab2/lab2.c
+++ cs580/cs580/labs/lab2/lab2.c
@@ -1,25 +1,17 @@
 #include<stdio.h>
 #include<limits.h>
 #include<stdlib.h>
-int random_between(int min, int max) {
-min=INT_MIN;max=INT_MAX;
- 
-  double r=(double) rand();
-double d = (double)max;
-double s= (int)r % (int)d ;
-printf("\n \n %f",r);
-printf("\n \n %f",s);
-}
 
-int main(){
-int f0=0;
-int f1=1;
+int main()
+{
+int f0=0; //first number in the fibonacci series
+int f1=1; //second number in the fibonacci series
 int random_number;
-int number=INT_MIN;
+//int number=INT_MIN;
 int i=0;
 int decimal_number=0;
-int random_number2=0;
-int count=0;
+
+//int count=0;
 unsigned int mask=1<<31; // make the number as 1 followed by 31 zeros
 //int bit_mask=255;
 int fnum=0;
@@ -31,9 +23,9 @@ int fnum=0;
 
 
 printf("Fibonacci Series first 20 numbers:");
-printf("\n%5d",f0);
+printf("\n%5d",f0); //print out first two numbers of the series
 printf("\n%5d",f1);
-for(i=0;i<18;i++)
+for(i=0;i<18;i++)   //remaining 18.
 	{
 	fnum=f0+f1;
 	printf("\n%5d",fnum);
@@ -53,25 +45,26 @@ decimal_number=2;
 
 printf("Decimal: %d\nBinray:\t",decimal_number);
 for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
+	if(decimal_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
 }
 printf("\n");
+//always reinitialize mask and the number
 mask=1<<31;
 decimal_number=255;
 
 printf("Decimal: %d\nBinary:\t",decimal_number);
 for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
+	if(decimal_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
 }
 
 printf("\n");
@@ -80,57 +73,58 @@ decimal_number=32;
 
 printf("Decimal: %d\nBinary:\t",decimal_number);
 for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
-}
+	if(decimal_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
+	}
 
 printf("\n");
 mask=1<<31;
-decimal_number=INT_MIN;
+decimal_number=-1;
 
 printf("Decimal: %d\nBinary:\t",decimal_number);
-for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
-}
+	for(;mask>0;mask=mask>>1){
+		if(decimal_number & mask){
+	printf("1");
+	}
+	else{
+		printf("0");
+	}
+	}
 printf("\n");
 mask=1<<31;
 decimal_number=INT_MAX;
 
 printf("Decimal: %d\nBinary:\t",decimal_number);
 for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
-}
+	if(decimal_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
+	}
 
 mask=1<<31;
 decimal_number=INT_MIN;
 printf("\n");
 printf("Decimal: %d\nBinary:\t",decimal_number);
 for(;mask>0;mask=mask>>1){
-if(decimal_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
-}
+	if(decimal_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
+	}
 
 //RANDOM BINARY NUMBERS:
 
 printf("\n\n\nRandom integer to Binary:");
+//randomize the seed. setting to system clock
 srand(time(0));
 random_number=rand();
 
@@ -139,12 +133,12 @@ mask=1<<31;
 printf("\n");
 printf("Decimal: %d\nBinary:\t",random_number);
 for(;mask>0;mask=mask>>1){
-if(random_number & mask){
-printf("1");
-}
-else{
-printf("0");
-}
+	if(random_number & mask){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
 }
 printf("\n");
 return 0;

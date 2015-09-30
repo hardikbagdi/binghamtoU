cdc79a63070cfd5d9f734977354ec93b31fd9b77
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-09-29 20:34:31 -0400

    Done with lab3

--- cs580/cs580/labs/lab3/lab3.c
+++ cs580/cs580/labs/lab3/lab3.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define EQUAL ==
 #define TRUE 1
 #define FALSE 0
-#define DEBUG(s) TRACE;fprintf(stderr,"%s",s);
+
+#if TRUE
+#define DEBUG(s) TRACE;fprintf(stderr,"\t %s",s);
 #define TRACE fprintf(stderr,"%d", __LINE__)
+#else
+#define DEBUG(s) 
+#define TRACE 
+#endif
+
 
 int myStrStr (char  haystack[], char needle[], char buffer[]){
 
@@ -13,16 +21,21 @@ int r;
 while(haystack[i]!='\0'){
 
 	if(haystack[i]==needle[j]){
-		printf("\nouter %c",needle[j]);
+		buffer[j]=needle[j];
+		//printf("\nouter %c",needle[j]);
 		j++;k=i;r=i;k++;
 			while(needle[j]!='\0'){
 				if(haystack[k]!=needle[j]) break;
-				printf("\n inner %c",needle[j]);
+				//printf("\n inner %c",needle[j]);
+				buffer[j]=needle[j];
 				j++;k++;
+				
 			}
 			if(needle[j]=='\0'){
-				printf("\nstring found at %d",r);
-				return 1;
+				buffer[j]=needle[j];
+			//	printf("\nstring found at %d",r);
+				
+				return 0;
 			}
 	j=0;
 	}
@@ -30,7 +43,7 @@ while(haystack[i]!='\0'){
 i++;
 }
 printf("String not found");
-return 0;
+return 1;
 
 }
 
@@ -38,9 +51,21 @@ return 0;
 int main(){
 int result;
 DEBUG("HELLO");
+
+//char haystack[20],needle[20],buffer[20];
 //fprintf(stderr,"%d", __LINE__);
-printf("\n\n\n\n\n");
-result =myStrStr("orange","ge","");
+printf("\n");
+
+
+char haystack[20];
+char needle[20];
+char buffer[10]="";
+printf("\nEnter the haystack string:");
+scanf("%s",haystack);
+printf("\nEnter the needle string:");
+scanf("%s",needle);
+result =myStrStr(haystack,needle,buffer);
+printf("\nHaystack:%s \nNeedle:%s \nBuffer:%s\n",haystack,needle,buffer);
 
 return 0;
 }

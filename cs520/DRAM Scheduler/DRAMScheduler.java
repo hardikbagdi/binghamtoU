c335d551be1cdedf516d596cf7d164c9898c40bf
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-11-13 03:03:22 -0500

    Fixed post long row read negative delay

--- cs520/DRAM Scheduler/DRAMScheduler.java
+++ cs520/DRAM Scheduler/DRAMScheduler.java
@@ -27,14 +27,25 @@ class Instruction{
 }
 public class DRAMScheduler{
 
-	public void doTiming(Instruction i){
-		System.out.println("A"+i.row);
+	public void doTiming(ArrayList<Instruction> list){
+		Instruction first = list.get(0);
+		System.out.println("A"+first.row);
+		int unkown_delay = 18;
 		System.out.println("I11");
-		System.out.println(i.type+i.col);
 		//always happens till this point
-
+		System.out.println(first.type+first.col);
+		for(int i=1;i<list.size();i++){
+		System.out.println(list.get(i).type+list.get(i).col);
+		}
 		//multiple instruction here can come if same row
+		if(unkown_delay-list.size()>0){
+		System.out.println("I"+(unkown_delay-list.size()));
+		//	System.out.println("SHit. this should never print");
+		}
+		else{
+			//System.out.println("SHit. this should never print");
 
+		}
 		//following always happens too
 		System.out.println("P");
 		System.out.println("I19");
@@ -64,6 +75,25 @@ public class DRAMScheduler{
 		// System.out.println(instructions.size());
 		
 		ArrayList<Instruction> sameRow = new ArrayList<Instruction>();
-		
+		while(counter<instructions.size()){
+
+			Instruction first = instructions.get(counter++);
+			sameRow.add(first);
+			if(counter==instructions.size()){
+			Dram.doTiming(sameRow);
+			break;
+			}
+			Instruction next= instructions.get(counter);
+			while(counter<(instructions.size()-1) && first.row.equals(next.row) ){
+				sameRow.add(next);
+				next=instructions.get(++counter);
+			}
+			//System.out.println(sameRow.size());
+			Dram.doTiming(sameRow);
+			sameRow.clear();
+
+			//if(counter>20){break;}
+
+		}
 	}
 }
---
+++ cs520/DRAM Scheduler/README.txt
@@ -0,0 +1,8 @@
+The makefile handles all commands.
+To run:
+
+make < input_file > output_file
+
+Programmed in Java.
+Read from STDIN and Writes to STDOUT.
+The output_file can be compared against reference directly.

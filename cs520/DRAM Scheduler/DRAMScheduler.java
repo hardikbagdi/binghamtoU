55029e94ef33ebdd15da986b750e3a845ccab100
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-11-17 22:28:43 -0500

    DRAM Scheduler logic update. Diff zero.

--- cs520/DRAM Scheduler/DRAMScheduler.java
+++ cs520/DRAM Scheduler/DRAMScheduler.java
@@ -26,24 +26,71 @@ class Instruction{
 	} 
 }
 public class DRAMScheduler{
-
+	static ArrayList<Instruction> instructions;
+	int globalCounter=0;
 	public void doTiming(ArrayList<Instruction> list){
+		int i=0,flag=0;;
+		int switchCount=0;
+		char currentSequence;
 		Instruction first = list.get(0);
 		System.out.println("A"+first.row);
 		int unkown_delay = 18;
 		System.out.println("I11");
 		//always happens till this point
-		System.out.println(first.type+first.col);
-		for(int i=1;i<list.size();i++){
-		System.out.println(list.get(i).type+list.get(i).col);
-		}
 		//multiple instruction here can come if same row
-		if(unkown_delay-list.size()>0){
-		System.out.println("I"+(unkown_delay-list.size()));
+		//System.out.println(first.type+first.col);
+		// for(i=1;i<list.size();i++){
+			
+		// 	System.out.println(list.get(i).type+list.get(i).col);
+		// }
+		globalCounter+=list.size();
+		while(i<list.size()){
+			currentSequence= list.get(i).type;
+			System.out.println(list.get(i).type+list.get(i).col);
+			if(list.get(i).type=='R') 
+				flag=2;
+			else
+				flag=0;
+			
+			i++;
+
+			while(i<list.size() && currentSequence == list.get(i).type){
+				if(list.get(i).type=='R') 
+					flag=2;
+				else
+					--flag;
+				System.out.println(list.get(i).type+list.get(i).col);
+					
+				i++;
+			}
+
+			if(i<list.size() && currentSequence=='R'){
+				if(list.get(i).type=='R') 
+					flag=2;
+				else
+					--flag;
+				switchCount++;
+				System.out.println("I3");
+			}
+
+
+		}
+		if(globalCounter>=(instructions.size())) return;
+		if((unkown_delay-list.size()-(3*switchCount))>0){
+			if((unkown_delay-list.size()-(3*switchCount))>flag){
+
+				System.out.println("I"+(unkown_delay-3*switchCount-list.size()));
 		//	System.out.println("SHit. this should never print");
+			}
+			else{
+				System.out.println("I"+flag);
+				
+			}
 		}
 		else{
 			//System.out.println("SHit. this should never print");
+				if(flag>0)
+				System.out.println("I"+flag);
 
 		}
 		//following always happens too
@@ -52,7 +99,7 @@ public class DRAMScheduler{
 	}
 	
 	public static void main(String[] args) {
-		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+		instructions = new ArrayList<Instruction>();
 		DRAMScheduler Dram = new DRAMScheduler();
 		int counter=0;
 		Scanner scanner;
